fcad57cd4a69c8a17b5763832beb83f29e6cd6d7
jxscabc <jxscqwe@163.com>
2019-12-02 18:46:36 +0800

    base code

---
+++ src/main/java/com/ldy/platform/MedicalDiagnosisPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.ldy.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDiagnosisPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalDiagnosisPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ldy/platform/MedicalDiagnosisPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldy.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalDiagnosisPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
