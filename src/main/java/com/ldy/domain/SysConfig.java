73da31b21b53159bfb447e14422bf8453794e45f
jxscabc <jxscqwe@163.com>
2020-01-21 12:13:07 +0800

    d

--- src/main/java/com/ldy/domain/SysConfig.java
+++ src/main/java/com/ldy/domain/SysConfig.java
@@ -1,40 +1,40 @@
 package com.ldy.domain;
 
-public class SysConfig {
-    private String variable;
-    private String value;
-    private String set_by;
+ public class SysConfig {
+        private String cid;
+        private String id;
+        private String userId;
 
-    public String getVariable() {
-        return variable;
-    }
+        public String getCid() {
+            return cid;
+        }
 
-    public void setVariable(String variable) {
-        this.variable = variable;
-    }
+        public void setCid(String cid) {
+            this.cid = cid;
+        }
 
-    public String getValue() {
-        return value;
-    }
+        public String getId() {
+            return id;
+        }
 
-    public void setValue(String value) {
-        this.value = value;
-    }
+        public void setId(String id) {
+            this.id = id;
+        }
 
-    public String getSet_by() {
-        return set_by;
-    }
+        public String getUserId() {
+            return userId;
+        }
 
-    public void setSet_by(String set_by) {
-        this.set_by = set_by;
-    }
+        public void setUserId(String userId) {
+            this.userId = userId;
+        }
 
-    @Override
-    public String toString() {
-        return "SysConfig{" +
-                "variable='" + variable + '\'' +
-                ", value='" + value + '\'' +
-                ", set_by='" + set_by + '\'' + "12"+
-                '}';
-    }
+        @Override
+        public String toString() {
+            return "SysConfig{" +
+                    "cid='" + cid + '\'' +
+                    ", id='" + id + '\'' +
+                    ", userId='" + userId + '\'' +
+                    '}';
+        }
 }
